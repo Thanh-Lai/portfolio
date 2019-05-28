c87578291c9c32242eda7eb2f7e3bacce00e9300
Thanh-Lai <tlai1122@gmail.com>
2019-05-27 23:32:01 -0400

    resume

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ class App extends Component {
         <Header data={this.state.resumeData.main} />
         <About data={this.state.resumeData.main} />
         <Resume data={this.state.resumeData.resume} />
-        <Portfolio data={this.state.resumeData.main} />
+        <Portfolio data={this.state.resumeData.portfolio} />
         <Quotes data={this.state.resumeData.quotes} />
         <Contact data={this.state.resumeData.main} />
         <ChatBox />
--- src/Components/About.js
+++ src/Components/About.js
@@ -13,14 +13,14 @@ class About extends Component {
          var zip = this.props.data.address.zip;
          var phone = this.props.data.phone;
          var email = this.props.data.email;
-         var resumeDownload = this.props.data.resumedownload;
+         // var resumeDownload = this.props.data.resumedownload;
       }
 
       return (
          <section id="about">
             <div className="row">
                <div className="three columns">
-                  <img className="profile-pic" src={profilepic} alt="Tim Baker Profile Pic" />
+                  <img className="profile-pic" src={profilepic} alt="Profile Pic" />
                </div>
                <div className="nine columns main-col">
                   <h2>About Me</h2>
@@ -39,7 +39,7 @@ class About extends Component {
                      </div>
                      <div className="columns download">
                         <p>
-                           <a href={resumeDownload} className="button"><i className="fa fa-download"></i>Download Resume</a>
+                           <a href="resume.pdf" download target="_blank" className="button"><i className="fa fa-download"></i>Download Resume</a>
                         </p>
                      </div>
                   </div>
