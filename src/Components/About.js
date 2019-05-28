35bcbdd300ff323851c4c0c91518ab00af5e42f9
Thanh-Lai <tlai1122@gmail.com>
2019-05-27 23:41:16 -0400

    download resume

--- src/Components/About.js
+++ src/Components/About.js
@@ -13,7 +13,7 @@ class About extends Component {
          var zip = this.props.data.address.zip;
          var phone = this.props.data.phone;
          var email = this.props.data.email;
-         // var resumeDownload = this.props.data.resumedownload;
+         var resumeDownload = this.props.data.resumedownload;
       }
 
       return (
@@ -39,7 +39,7 @@ class About extends Component {
                      </div>
                      <div className="columns download">
                         <p>
-                           <a href="resume.pdf" download target="_blank" className="button"><i className="fa fa-download"></i>Download Resume</a>
+                           <a href={resumeDownload} download="Thanh-Lai-Resume" className="button"><i className="fa fa-download"></i>Download Resume</a>
                         </p>
                      </div>
                   </div>
