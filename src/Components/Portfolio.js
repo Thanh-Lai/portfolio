096b0f237fde0501e47aef5fc59bcf6e51f90e49
Thanh-Lai <tlai1122@gmail.com>
2019-06-02 12:52:48 -0400

    target blank on project links

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -8,7 +8,7 @@ class Portfolio extends Component {
         var projectImage = 'images/portfolio/'+projects.image;
         return <div key={projects.title} className="columns portfolio-item">
            <div className="item-wrap">
-            <a href={projects.url} title={projects.title}>
+            <a href={projects.url} target="_blank" rel="noopener noreferrer" title={projects.title}>
                <img alt={projects.title} src={projectImage} />
                <div className="overlay">
                   <div className="portfolio-item-meta">
