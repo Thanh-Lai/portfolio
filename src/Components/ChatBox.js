5e874efbf8cbfad741d890a0b7be8b88670703af
Thanh-Lai <tlai1122@gmail.com>
2019-09-10 23:41:53 -0400

    added toggle chatbot on load

--- src/Components/ChatBox.js
+++ src/Components/ChatBox.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Interactions } from 'aws-amplify';
-import { Widget, addResponseMessage } from 'react-chat-widget';
+import { Widget, addResponseMessage, toggleWidget } from 'react-chat-widget';
 import 'react-chat-widget/lib/styles.css';
 import logo from '../logo.png';
 
@@ -11,6 +11,7 @@ class ChatBox extends Component {
         addResponseMessage("Welcome! I am Thanh Bot, a custom built AI chatbot that specializes in answering interview questions for Thanh and other questions about Thanh. I also tell Chuck Norris jokes!");
         addResponseMessage("Example: 'Which languages do you know?'");
         addResponseMessage("Example: 'Tell me a joke'");
+        toggleWidget()
       }
 
     async handleNewUserMessage (newMessage) {
@@ -21,6 +22,7 @@ class ChatBox extends Component {
       }
 
       render() {
+  
         return (
             <Widget
                 handleNewUserMessage={this.handleNewUserMessage}
