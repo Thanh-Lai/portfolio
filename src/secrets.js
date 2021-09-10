9606b5df75ad31512dc47589fd2cf3114fc63be1
Thanh Lai <34387230+Thanh-Lai@users.noreply.github.com>
2021-09-10 03:56:09 -0400

    Delete secrets.js

--- src/secrets.js
+++
@@ -1,9 +0,0 @@
-const AWS_ACCESS_KEY = 'AKIAWDALU4BZDBTLV5NP';
-const AWS_SECRET_KEY = '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';
-const AWS_REGION = 'us-east-1';
-
-module.exports = {
-    AWS_ACCESS_KEY,
-    AWS_SECRET_KEY,
-    AWS_REGION
-};
