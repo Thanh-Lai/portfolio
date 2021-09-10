f12625568d4f249f8e9c0145454e9466c6569d08
Thanh-Lai <tlai1122@gmail.com>
2021-09-10 16:58:37 -0400

    clean up

---
+++ src/secrets.js
@@ -0,0 +1,9 @@
+const AWS_ACCESS_KEY = 'AKIAWDALU4BZKNFAPEHR';
+const AWS_SECRET_KEY = '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';
+const AWS_REGION = 'us-east-1';
+
+module.exports = {
+    AWS_ACCESS_KEY,
+    AWS_SECRET_KEY,
+    AWS_REGION
+};
